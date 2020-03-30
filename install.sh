3617ae5bf871310e121b211be40ffb13b69033e7
John Nduli <yohanaizraeli@gmail.com>
2020-03-30 19:09:37 +0300

    Complete support for vim setup

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # dotfiles folder
 DOTFILES_DIR=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
+
 # check that os is archlinux
 is_archlinux_or_exit() {
     if [ -f "/etc/arch-release" ]; then
@@ -21,7 +22,7 @@ install_packages() {
     packages+=' xfce4 dmenu xautolock'
 
     # editors I use
-    packages+=' gvim neovim python-pynvim git'
+    packages+=' gvim neovim python-pynvim git curl'
 
     pacman -Sy --noconfirm "$packages"
 }
@@ -40,30 +41,41 @@ replace_symlinks_or_move_files_to_old(){
         mv "${1}" "${1}.old"
     fi
     echo "Symlinking ${1} to ${2}"
-    ln -s "${2}" "${1}"
+    ln --force --symbolic "${2}" "${1}"
 }
 
 vim_setup(){
+    # set up .vimrc and init.vim
     local vimrc_path="$HOME/.vimrc"
-    local dotfile_vimrc="${DOTFILES_DIR}/.vimrc"
+    local dotfile_vimrc="${DOTFILES_DIR}/editors/vimrc"
     replace_symlinks_or_move_files_to_old "$vimrc_path" "$dotfile_vimrc"
     local neovim_init_path="$HOME/.config/nvim/init.vim"
-    local dotfile_neovim="${DOTFILES_DIR}/nvim-init.vim"
+    local dotfile_neovim="${DOTFILES_DIR}/editors/nvim-init.vim"
     replace_symlinks_or_move_files_to_old "$neovim_init_path" "$dotfile_neovim"
-    # set up .vimrc and init.vim
     # set up Plug (plugin manager)
-    # vim -c ':PlugInstall' -c 'qa!'
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    # install vim plugins
+    vim -c ':PlugInstall' -c 'qa!'
+}
+
+i3_setup() {
+    packages+='i3 feh scrot'
 }
 
 show_help() {
     cat <<EOF
 This installs dependencies for various files in the dotfiles.
 It also sets up symlinks to the various dotfiles in this repository.
+
+The following are setup:
+    vim, neovim, and their respective plugins
 EOF
 }
 
+# Tested functions that are ok
 # install_packages
-vim_setup
+# vim_setup
 
 # For i3, the following need to be installed in archlinux
 #redshift-gtk

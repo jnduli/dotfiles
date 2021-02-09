562fe62f9e28f5b1a75b83043b1514d9fa85b06b
John Nduli <yohanaizraeli@gmail.com>
2021-02-09 15:54:55 +0300

    fix dishes with siz moving out

--- scripts/dishes.sh
+++ scripts/dishes.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-DishesRoter=(Purity John Boni)
-dayOwner=$(($(date +%j) % 3)) # 0 indexing
+# DishesRoter=(Purity John Boni)
+DishesRoter=(John Boni)
+dayOwner=$((10#$(date +%j) % ${#DishesRoter[@]})) # 0 indexing
 echo "${DishesRoter[$dayOwner]} to clean dishes"

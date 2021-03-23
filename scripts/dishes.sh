f4e8c4dc4e48bcde5c3967a67f75b82479ed12d4
John Nduli <yohanaizraeli@gmail.com>
2021-03-23 04:58:24 +0300

    add purity to dishes list

--- scripts/dishes.sh
+++ scripts/dishes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # DishesRoter=(Purity John Boni)
-DishesRoter=(John Boni)
+DishesRoter=(John Purity Boni)
 dayOwner=$((10#$(date +%j) % ${#DishesRoter[@]})) # 0 indexing
 echo "${DishesRoter[$dayOwner]} to clean dishes"

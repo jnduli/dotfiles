6ecee87eebb29021965f14dcf6126ecda5562c81
John Nduli <yohanaizraeli@gmail.com>
2022-01-21 20:09:08 +0300

    change font to ubuntu mono

---
+++ scripts/pto_handling.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# I have 1.75 days of PTO per year
+# Roll over happens for 8 days per yeear
+# I'm looking for a way to automatically notify myself when I need to have more fere dayads.
+# I can set this script up in my server ahd have an email sent out whenever my days exceed 5 days, so that I can schedule more.
+
+# PErhaps do a daemon with this, let's see how this would work.
+
+# PTO_Days_taken, I need to update this every time I take days off
+PTO_days_taken=14
+Sick_off_days_taken=14
+
+Sick_off_days=14
+
+previous_years_carried_over_days=9
+
+# approximation since bash doesn't handle floats
+accumulated_days=$((175 * $(date +%-m) / 100 ))
+
+untaken_pto_days=$(( accumulated_days - PTO_days_taken ))
+untaken_sick_off=$(( Sick_off_days - Sick_off_days_taken ))
+echo "Please schedule following days for PTO: " $untaken_pto_days
+echo "If sick, I have the following days: " $untaken_sick_off

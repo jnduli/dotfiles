484fceb1948e881774e8ff12b514f68e34eb4380
John Nduli <yohanaizraeli@gmail.com>
2022-04-08 09:02:10 +0300

    modify vimrc to get github links and update pto script

--- scripts/pto_handling.sh
+++ scripts/pto_handling.sh
@@ -7,17 +7,17 @@
 # PErhaps do a daemon with this, let's see how this would work.
 
 # PTO_Days_taken, I need to update this every time I take days off
-PTO_days_taken=14
-Sick_off_days_taken=14
+PTO_days_taken=12
+Sick_off_days_taken=0
 
 Sick_off_days=14
 
-previous_years_carried_over_days=9
+previous_years_carried_over_days=8
 
 # approximation since bash doesn't handle floats
 accumulated_days=$((175 * $(date +%-m) / 100 ))
 
-untaken_pto_days=$(( accumulated_days - PTO_days_taken ))
+untaken_pto_days=$(( previous_years_carried_over_days + accumulated_days - PTO_days_taken ))
 untaken_sick_off=$(( Sick_off_days - Sick_off_days_taken ))
 echo "Please schedule following days for PTO: " $untaken_pto_days
 echo "If sick, I have the following days: " $untaken_sick_off

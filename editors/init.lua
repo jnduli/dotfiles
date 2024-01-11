9a832ed8fd146931ab8bea20fb55f802a34f30b6
John Nduli <rookie101@jnduli.co.ke>
2024-01-11 18:31:20 +0300

    use ioveska nerd fron and add timeout to Format LSP

--- editors/init.lua
+++ editors/init.lua
@@ -429,7 +429,7 @@ local on_attach = function(_, bufnr)
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
+    vim.lsp.buf.format{ timeout_ms = 2000 }
   end, { desc = 'Format current buffer with LSP' })
 end
 

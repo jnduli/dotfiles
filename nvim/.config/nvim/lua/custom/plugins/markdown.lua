868f772d00dbce17c22be08d3e74a76bb90dd129
John Nduli <rookie101@jnduli.co.ke>
2025-05-05 13:14:45 +0100

    feat: support for basic stats in md todos

--- nvim/.config/nvim/lua/custom/plugins/markdown.lua
+++ nvim/.config/nvim/lua/custom/plugins/markdown.lua
@@ -1,8 +1,33 @@
 vim.treesitter.language.register("markdown", "vimwiki")
-vim.g.markdown_folding = 1
+
+local log_level = "info" -- change to debug for more logs
+local log = require("plenary.log").new({
+  -- logs stored in echo stdpath("log")/custom_markdown.log
+  plugin = "custom_markdown",
+  level = log_level,
+})
 
 DONE_CHECKLIST = "- [X]"
 OPEN_CHECKLIST = "- [ ]"
+
+local CHECKLIST_STATUS = {
+  OPEN = 1,
+  DONE = 2,
+  UNKNOWN = 3,
+}
+
+local function str_to_checklist(raw_string)
+  local status = string.sub(raw_string, 1, string.len(DONE_CHECKLIST))
+  local status_obj = CHECKLIST_STATUS.UNKNOWN
+  if status == DONE_CHECKLIST then
+    status_obj = CHECKLIST_STATUS.DONE
+  elseif status == OPEN_CHECKLIST then
+    status_obj = CHECKLIST_STATUS.OPEN
+  else
+    return nil
+  end
+  return { status = status_obj, content = raw_string }
+end
 
 local function move_up_checklist_item(lnum)
   if not lnum then
@@ -11,18 +36,22 @@ local function move_up_checklist_item(lnum)
   local up_lnum = lnum - 1
   while up_lnum > 0 do
     local current_line = vim.api.nvim_buf_get_lines(0, up_lnum, up_lnum + 1, false)[1]
-    local status = string.sub(current_line, 1, string.len(DONE_CHECKLIST))
-    if status == DONE_CHECKLIST then
+    local checklist = str_to_checklist(current_line)
+    if checklist == nil then
       break
     end
-    if status ~= DONE_CHECKLIST and status ~= OPEN_CHECKLIST then
+    if checklist.status == CHECKLIST_STATUS.DONE then
       break
     end
     up_lnum = up_lnum - 1
   end
 
+  if lnum == up_lnum + 1 then -- same line
+    return
+  end
+
   local line = vim.api.nvim_buf_get_lines(0, lnum, lnum + 1, false)[1]
-  vim.print("Moving " .. line .. " from: " .. lnum .. " to: " .. up_lnum)
+  log.debug("Moving " .. line .. " from: " .. lnum .. " to: " .. up_lnum)
   vim.api.nvim_buf_set_lines(0, lnum, lnum + 1, false, {})
   vim.api.nvim_buf_set_lines(0, up_lnum + 1, up_lnum + 1, false, { line })
 end
@@ -44,7 +73,6 @@ local function toggle_markdown_checklist()
     dash_col + string.len(DONE_CHECKLIST) - 1,
     {}
   )[1]
-  vim.print(current_check)
   local next_content = next_checklist_table[current_check]
   if next_content == nil then
     return
@@ -63,6 +91,50 @@ local function toggle_markdown_checklist()
   vim.fn.setpos(".", current_cursor)
 end
 
+local STATS_NAMESPACE_ID = vim.api.nvim_create_namespace("StatsVirtualTextNamespace")
+local STATS_HIGHLIGHT_GROUP = "StatsHighlight"
+local global_stats_ext_mark = nil
+local global_top_line_index = nil
+
+vim.api.nvim_set_hl(0, STATS_HIGHLIGHT_GROUP, { fg = "black", bg = "yellow", bold = true })
+
+local function stats()
+  local buf = vim.api.nvim_get_current_buf()
+
+  local winline = vim.fn.winline() -- get current line relative to the top of the window
+  local current_cursor = vim.fn.getcurpos()
+  local current_line = current_cursor[2] -- get current line relative to the whole file
+  local line_with_stats = current_line - winline
+  if global_top_line_index == line_with_stats and not vim.bo.modified then
+    return
+  end
+
+  if global_stats_ext_mark ~= nil then
+    vim.api.nvim_buf_del_extmark(buf, STATS_NAMESPACE_ID, global_stats_ext_mark)
+  end
+
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local total = 0
+  local done = 0
+  for _, line in ipairs(lines) do
+    local checklist_obj = str_to_checklist(line)
+    if checklist_obj and checklist_obj.status == CHECKLIST_STATUS.DONE then
+      done = done + 1
+      total = total + 1
+    elseif checklist_obj and checklist_obj.status == CHECKLIST_STATUS.OPEN then
+      total = total + 1
+    end
+  end
+  local ratio = done / total
+  local stats_msg = string.format("%d/%d, %.2f", done, total, ratio)
+
+  global_top_line_index = line_with_stats
+  global_stats_ext_mark = vim.api.nvim_buf_set_extmark(buf, STATS_NAMESPACE_ID, line_with_stats, 0, {
+    virt_text = { { stats_msg, STATS_HIGHLIGHT_GROUP } },
+    virt_text_pos = "eol_right_align",
+  })
+end
+
 local markdown_group = vim.api.nvim_create_augroup("markdown_group", { clear = true })
 vim.api.nvim_create_autocmd("Filetype", {
   pattern = { "markdown", "vimwiki" },
@@ -72,6 +144,14 @@ vim.api.nvim_create_autocmd("Filetype", {
     vim.keymap.set("n", "<C-u>", move_up_checklist_item, { silent = true, desc = "move checklist item to top" })
   end,
   group = markdown_group,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorMoved" }, {
+  pattern = { "*.md" },
+  group = markdown_group,
+  callback = function()
+    stats()
+  end,
 })
 
 --
@@ -85,8 +165,8 @@ return {
     init = function()
       vim.g.vimwiki_list = {
         {
-          path = "~/vimwiki",
-          path_html = "~/vimwiki/public_html",
+          path = "~/vimwiki/wiki",
+          path_html = "~/vimwiki/wiki/public_html",
           auto_tags = 1,
           auto_diary_index = 1,
           syntax = "markdown",

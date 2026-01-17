487935a691f1dfc7776864760bba4f835b0c561c
John Nduli <rookie101@jnduli.co.ke>
2026-01-17 16:14:53 +0000

    Stop failed/inactive scopes before attempting to launch jobs in matumizi
    For example, when running chromium in the systemd user context, I ran
    out of memory. This caused the `matumizi_chromium.scope` to be in a
    failed state, so loading it up caused it to fail. A better approach is
    to first check for this state, stop and reset the scopes, then attempt
    to start the tasks.

--- scripts/dot-local/bin/,matumizi.sh
+++ scripts/dot-local/bin/,matumizi.sh
@@ -10,4 +10,18 @@ memory_chosen=$(printf "%s\n" "${MEMORY_OPTIONS[@]}" |  rofi -font 'iosevka 20' 
 
 program=$(dmenu_path | rofi -font 'iosevka 20' -i -dmenu  -matching fuzzy -p "$memory_chosen;$CPU_WEIGHT:")
 unit_name="matumizi_${program}"
-systemd-run --scope --unit="$unit_name" --user --property="$memory_chosen" --property="$CPU_WEIGHT"  "$program"
+status=$(systemctl  --user is-active $unit_name || true)
+
+echo "Found status for $unit_name as $status: NOW"
+
+if [[ $status == "active" || $status == "reloading" || $status == "activating" ]]; then
+    echo "Unit $unit_name has the $status: $status; Not doing anything"
+else
+    if [[ $status == "failed" || $status == "inactive" ]]; then
+        echo "Stopping unit $unit_name"
+        systemctl --user stop $unit_name.scope || true
+        systemctl --user reset-failed $unit_name.scope || true
+    fi
+    echo "Starting unit $unit_name"
+    systemd-run --scope --unit="$unit_name" --user --property="$memory_chosen" --property="$CPU_WEIGHT"  "$program"
+fi

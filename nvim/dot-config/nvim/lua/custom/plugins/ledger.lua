967021205c58992abd62986cf2c574db49b2565f
John Nduli <rookie101@jnduli.co.ke>
2026-02-15 11:35:39 +0000

    update init to latest kickstart.nvim

--- nvim/dot-config/nvim/lua/custom/plugins/ledger.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/ledger.lua
@@ -13,8 +13,44 @@ vim.api.nvim_create_autocmd("Filetype", {
   end,
   group = ledger_group,
 })
-vim.keymap.set("n", "<leader>o$", ":tabnew ~/vimwiki/ledger/blackbook.ledger<cr>", { desc = "open ledger file" })
+vim.keymap.set("n", "<leader>o$", ":tabnew ~/vimwiki/ledger/blackbook.beancount<cr>", { desc = "open ledger file" })
 
 return {
   "ledger/vim-ledger",
+  {
+    "hxueh/beancount.nvim",
+    ft = { "beancount", "bean" },
+    dependencies = {
+      {
+        "saghen/blink.cmp",
+        optional = true,
+        opts = function(_, opts)
+          table.insert(opts.sources.default, "beancount")
+          opts.sources.providers = opts.sources.providers or {}
+          opts.sources.providers.beancount = {
+            name = "beancount",
+            module = "beancount.completion.blink",
+            score_offset = 100,
+            opts = {
+              trigger_characters = { ":", "#", "^", '"', " " },
+            },
+          }
+          return opts
+        end,
+      },
+      {
+        "L3MON4D3/LuaSnip",
+      },
+    },
+    config = function()
+      require("beancount").setup({})
+      -- Treesitter setup
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "beancount" },
+        highlight = { enable = true },
+        incremental_selection = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }

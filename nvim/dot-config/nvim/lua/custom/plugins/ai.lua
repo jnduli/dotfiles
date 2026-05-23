11077296d4422353e971ce7eaac4f747cb217ddc
John Nduli <rookie101@jnduli.co.ke>
2026-05-23 10:37:24 +0100

    remove copilot

--- nvim/dot-config/nvim/lua/custom/plugins/ai.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/ai.lua
@@ -40,44 +40,4 @@ return {
       },
     },
   },
-  {
-    "zbirenbaum/copilot.lua",
-    dependencies = {
-      "copilotlsp-nvim/copilot-lsp",
-      init = function()
-        vim.g.copilot_nes_debounce = 500
-        vim.lsp.enable("copilot_ls")
-      end,
-    },
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        workspace_folders = {
-          "/home/rookie/vimwiki/",
-        },
-        filetypes = {
-          yaml = false,
-          markdown = false,
-          help = false,
-          gitcommit = false,
-          gitrebase = false,
-          hgcommit = false,
-          svn = false,
-          cvs = false,
-          vimwiki = false,
-          ["."] = false,
-        },
-        nes = {
-          enabled = true, -- requires copilot-lsp as a dependency
-          auto_trigger = true,
-          keymap = {
-            accept_and_goto = "<Tab>",
-            accept = false,
-            dismiss = "<Esc>",
-          },
-        },
-      })
-    end,
-  },
 }
